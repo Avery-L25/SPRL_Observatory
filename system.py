37e447fb827908335a69b7fb0e7402e8fb7d340e
Avery-L25 <amland@umich.edu>
2025-06-09 12:51:42 -0400

    Linting and comments Ignored unnecessary files

--- system.py
+++ system.py
@@ -1,45 +1,52 @@
+#!/usr/bin/env python3
+
 import datetime
 import numpy as np
 from ischedule import run_loop, schedule
 from Data_processing.image_processing import Image
-from Data_processing.hdf import hdf
-from Data_processing.transmission.file_transmission_2 import upload_file_to_drive
+# from Data_processing.transmission.file_transmission_2 import upload_file_to_drive # NOTE not planning on uploading to drive this is a placeholder
+
 from Sensors.barom_therm_data_collection import temp_n_pres
 from Sensors.mag_data import mag_data
 from Sensors.camera.shot import shot
 
-T = 10
-camera_period = 0 # a counter for camera's period 
-img = Image(np.zeros((512, 512, 3)))
-cur_day = datetime.date.today()
+# Variable initialization
+T = 10  # When to take image
+camera_period = 0  # a counter for camera's period
+img = Image(np.zeros((512, 512, 3)))  # blank image
+cur_day = datetime.date.today()  # keep track of current day
+# NOTE: will be replaced
 
 folder_id = "1o4uRjhgtwmRvK-3zxdAHeIlSxOJW4nqL"
 hdf_path = 'test.hdf5'
 
+
 def cam_off():  # Turn off the cam
     None
+
 
 def read_data(cam_flag):    # instruct sensors to read current data
     # None will be replaced to be the sensor's program
     mag = mag_data()
     temp, pres = temp_n_pres()
-#    temp, pres, mag = [None]*2, None, [None]*3
+    # temp, pres, mag = [None]*2, None, [None]*3
     gps = None
 
-    if cam_flag:    # separate cam reading due to different frequency from sensors
-        img = shot()#np.ones((512, 512, 3))
+    if cam_flag:  # separate cam reading due to different frequency sensors
+        img = shot()  # np.ones((512, 512, 3))
     else:
         img = np.zeros((512, 512, 3))
     return mag, pres, temp, gps, img
 
+
 def timer():
     global T, camera_period, img, cur_day
- 
-    if (datetime.date.today() - cur_day).days <= 0: # new day
-#        upload_file_to_drive(hdf_path, folder_id)
+
+    if (datetime.date.today() - cur_day).days <= 0:  # new day
+        # upload_file_to_drive(hdf_path, folder_id)
         cur_day = datetime.date.today()
 
-    if datetime.datetime.now().hour > 18:   # Can be skipped if there is no new img
+    if datetime.datetime.now().hour > 18:  # Can be skipped if no new img
         if img.aurora_detection():  # if there is aurora detected
             T = 10
             print('no aurora')
@@ -51,7 +58,7 @@ def timer():
         print('daytime')
 
     camera_period += 10
-        
+
 
 def data_processing():
     global T, camera_period
@@ -63,12 +70,11 @@ def data_processing():
     img.aurora_detection()
     hdf(mag, pres, temp, gps, img.img)  # input data into database
 
+
 if __name__ == '__main__':
     # run the program with period = 10 sec
-#    schedule(timer, interval=2)
-    schedule(data_processing, interval=2)  
+    # schedule(timer, interval=2)
+    schedule(data_processing, interval=2)
     run_loop(return_after=3)
-    
-#    upload_file_to_drive(hdf_path, folder_id)
-    
 
+    # upload_file_to_drive(hdf_path, folder_id)
