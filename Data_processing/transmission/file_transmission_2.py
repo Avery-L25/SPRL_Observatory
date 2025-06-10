5fa5765a62f6106a85ecf64582b14c6f26325a2a
Avery-L25 <amland@umich.edu>
2025-06-10 09:46:08 -0400

    linting

--- Data_processing/transmission/file_transmission_2.py
+++ Data_processing/transmission/file_transmission_2.py
@@ -1,29 +1,31 @@
 import os.path
 import h5py
 import numpy as np
-import time  
+import time
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload 
+from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+
 
 def create_hdf5_file(file_name):
     """Creates an HDF5 file and writes random data."""
     with h5py.File(file_name, "w") as f:
-        data = np.random.rand(100, 100)  
+        data = np.random.rand(100, 100)
         f.create_dataset("random_data", data=data)
     print(f"{file_name} created successfully.")
+
 
 def upload_file_to_drive(file_name, folder_id):
     """Uploads a file to Google Drive in the specified folder."""
     creds = None
     token_path = 'Data_processing/transmission/token_2.json'
     creds_path = 'Data_processing/transmission/credentials.json'
-    
+
     if os.path.exists(token_path):
         creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
@@ -44,28 +46,32 @@ def upload_file_to_drive(file_name, folder_id):
 
         file_metadata = {
             "name": file_name,
-            "parents": [folder_id],  
+            "parents": [folder_id],
         }
         media = MediaFileUpload(file_name, mimetype="application/x-hdf5")
         file = service.files().create(
             body=file_metadata, media_body=media, fields="id"
         ).execute()
 
-        print(f"File ID: {file['id']} uploaded successfully to folder {folder_id}.")
+        print(f"File ID: {file['id']} uploaded successfully to folder"
+              f"{folder_id}.")
     except HttpError as error:
         print(f"An error occurred: {error}")
 
+
 def main():
-    """Creates an HDF5 file, uploads it to Google Drive, and repeats every 10 seconds."""
-    folder_id = "1o4uRjhgtwmRvK-3zxdAHeIlSxOJW4nqL"  
-    file_counter = 1  
+    """Creates an HDF5 file, uploads it to Google Drive,
+    and repeats every 10 seconds."""
+    folder_id = "1o4uRjhgtwmRvK-3zxdAHeIlSxOJW4nqL"
+    file_counter = 1
 
     while True:
         file_name = f"random_data_{file_counter}.h5"
         create_hdf5_file(file_name)
         upload_file_to_drive(file_name, folder_id)
         file_counter += 1
-        time.sleep(10)  
+        time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
