d9df4714ace015077ab52c07bfd2b9cc02970235
Avery-L25 <amland@umich.edu>
2025-07-18 14:23:21 -0400

    Removed timer function. -> moved the T variable manager into data processing -> upload_data function is now incharge of uploading file via rsyn cam_flag is now defined in the data_processing function in order to manage when to take an image more accurately
    replaced ischedule library with schedule library to manage more functions at a time
    
    added to cam_off function to prevent taking pictures during the day

--- system.py
+++ system.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+# import necessary libraries
+import os
 import datetime
+import time
 import numpy as np
-from ischedule import run_loop, schedule
+#// from ischedule import run_loop, schedule
+import schedule
 from Data_processing.image_processing import Image
 # // from Data_processing.transmission.file_transmission_2 import upload_file_to_drive #NOTE not planning on uploading to drive this is a placeholder
 from Data_processing.hdf import hdf
@@ -13,18 +17,29 @@ from Sensors.camera.main import shot
 
 # Variable initialization
 T = 10  # When to take image
+xrun = 5  # todo how often to take data
 camera_period = 0  # a counter for camera's period
 img = Image(np.zeros((512, 512, 3)))  # blank image
 cur_day = datetime.date.today()  # keep track of current day
-# NOTE: cur_day should be replaced when we have a better method to dump info
-# * live_plot = Live_plotting() # in used coded
+cameraoff = False  # when True camera will not take picutre during the day
 
-folder_id = "1o4uRjhgtwmRvK-3zxdAHeIlSxOJW4nqL"  # todo choose folder
-hdf_path = 'test.hdf5'  # todo choose path
+
+
+# Working directory and file
+wdir = os.getcwd()
+folder = cur_day.strftime('%y%B')
+#// folder_id = "1o4uRjhgtwmRvK-3zxdAHeIlSxOJW4nqL"  # ! this should not be necessary
+hdf_path = cur_day.strftime('%d_%m_%y.hdf5')  # ! this should be the file name and hdf_path should become a combination of folder and file when implemented onto raspi
+# todo 2 paths? the raspi and the one on crabyss
 
 
 def cam_off():  # Turn off the cam  # todo turn off camera during the dayS
-    None
+    global cameraoff
+    curtime = datetime.now()
+    if curtime.hour > 7 and curtime.hour < 15:
+        cameraoff = True
+    else:
+        cameraoff = False
 
 
 def read_data(cam_flag):    # instruct sensors to read current data
@@ -33,51 +48,56 @@ def read_data(cam_flag):    # instruct sensors to read current data
     temp, pres = temp_n_pres()
     gps = None  # todo complete gps code
 
-    if cam_flag:  # separate cam reading due to different frequency sensors # ! does this work without a camflag ????
+    if cam_flag:  # takes an image if the camera period is complete
         img = shot()  # ! check integration of proper shot()
     else:
-        img = np.zeros((512, 512, 3), dtype=np.uint8)
+        img = np.zeros((512, 512, 3), dtype=np.uint8)  # ? can this be made into None and have no information saved???
+
     return mag, pres, temp, gps, img
 
 
-def timer():   # todo revamp systems for our personal time needs
-    global T, camera_period, img, cur_day
-
-    if (datetime.date.today() - cur_day).days <= 0:  # new day
-        # upload_file_to_drive(hdf_path, folder_id)  # todo how do we want to upload files???
-        cur_day = datetime.date.today()  # todo update when appropriate
-
-    if datetime.datetime.now().hour > 18:  # Can be skipped if no new img
-        if img.aurora_detection():  # if there is aurora detected
-            T = 10
-            print('aurora present')
-        else:
-            T = 5*60
-            print('no aurora')
-        #* camera_period += 10
-    else:
-        cam_off()  # * instead they set # camera_period = 1
-        print('daytime')
-
-    camera_period += 10 #* see above
-
+def upload_data():
+    global hdf_path
+    print('uploading data to')
+    # todo os.system(f'rsync -ahP {hdf_path} *USER*@crabyss.engin.umich.edu:{directory to save}') 
+    #! ensure that the delete flag is here unless addressed seperately
 
 def data_processing():
-    global T, camera_period
-    if camera_period >= T:  # it the time to take picture
+    global T, xrun, camera_period, cameraoff
+    if cameraoff is True:
+        cam_flag = False
+    elif camera_period >= T:  # it the time to take picture
         camera_period = 0
+        cam_flag = True
         img.pre = img.img
-    mag, pres, temp, gps, img.img = read_data(bool(camera_period))
+    else:
+        camera_period += xrun  # update counter by the run period
+        cam_flag = False  # ! can be moved to where the camera_period is updated
+    mag, pres, temp, gps, img.img = read_data(bool(cam_flag))
     img.resize()
-    img.aurora_detection() #* added "is_aurora =" to the beginning
+    is_aurora = img.aurora_detection()  # is there an aurora present
+    if is_aurora is True:  # if yes, camera takes a photo every 10 seconds
+        T = 10
+        print('aurora present')
+    elif is_aurora is False:  # if no, camera takes a photo every 5 minutes
+        T = 300
+        print('no aurora')
     hdf(mag, pres, temp, gps, img.img)  # input data into database
-    #* live_plot.plotting({'x': mag[0],'y': mag[1],'z': mag[2]}, {'in': temp[1], 'out': temp[0]}, pres, img.img, is_aurora)
+    # * live_plot.plotting({'x': mag[0],'y': mag[1],'z': mag[2]}, {'in': temp[1], 'out': temp[0]}, pres, img.img, is_aurora)
 
+
+# initializes scheduling
+schedule.every(xrun).seconds.do(data_processing)  # collect data every 'xrun'
+schedule.every().day.at("16:00").do(upload_data)  # upload hdf5 file at 4pm
+schedule.every().day.at("08:00").do(cam_off)  # turn camera off after 8am
+schedule.every().day.at("20:00").do(cam_off)  # turn camera on after 8pm
 
 if __name__ == '__main__':
     # run the program with period = XX sec
+    schedule.run_pending()
+    time.sleep(1)
     # schedule(timer, interval=2) # ! this wasnt used before?????
-    schedule(data_processing, interval=2)  # todo choose our intervals
-    run_loop(return_after=3)  # todo choose our intervals
+    # //schedule(data_processing, interval=2)  # todo choose our intervals
+    # //run_loop(return_after=3)  # todo choose our intervals
 
     # upload_file_to_drive(hdf_path, folder_id)   # todo our upload
